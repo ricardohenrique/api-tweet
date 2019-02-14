3de0cf4553f415319940a33c07784e7027164aa8
ricardohenrique <ricardo.hb@outlook.com>
2019-02-14 01:04:31 -0300

    new way to create tweets

--- app/Service/TweetService.php
+++ app/Service/TweetService.php
@@ -70,15 +70,90 @@ class TweetService
      */
     public function getSlicedRandomTweet(object $data): array
     {
-        $rand = rand(0, count($data));
+        $rand = rand(0, (count($data) - 1));
         $tweet = $data[$rand];
         $tweets = [];
 
-        for ($i=0; $i < ceil(strlen($tweet) / self::TWEETNUMBER); $i++) {
-            $tweets[] = substr($tweet, (self::TWEETNUMBER * $i), (self::TWEETNUMBER * ($i + 1)));
+        $words = $this->getWordsFromString($tweet);
+        $finalData = $this->makeTweetsFromArrayWords($words);
+
+        dd($finalData);
+
+        return $finalData;
+    }
+
+    /**
+     * Make Tweets From an Array Words
+     * @access public
+     * @param array $words
+     * @return array
+     */
+    public function makeTweetsFromArrayWords(array $words): array
+    {
+        $dataReturn     = [];
+        $tweetWords     = [];
+        $validateLength = 0;
+        $tweetNumber    = 1;
+
+        foreach ($words as $key => $word) {
+            $nextString     = array_merge($tweetWords, [$word]);
+            $validateLength = mb_strlen(implode(' ', $nextString), 'utf8');
+            
+            if($validateLength >= self::TWEETNUMBER) {
+                $validateLength = 0;
+                $dataReturn[]   = $this->buildTweet($tweetWords, $tweetNumber);
+                $tweetWords     = [];
+                $tweetNumber++;
+            }
+            $tweetWords[] = $word;
+
+            if(end($words) == $word){
+                $dataReturn[] = $this->buildTweet($tweetWords, $tweetNumber);
+            }
         }
 
-        return $tweets;
+        return $dataReturn;
+    }
+
+    public function buildTweet(array $array, int $number): string
+    {
+        return "Tweet #$number: " . implode(' ', $array);
+    }
+
+    /**
+     * Get Words From String
+     * @access public
+     * @param string $string
+     * @return array
+     */
+    public function getWordsFromString(string $string): array
+    {
+        $arrayWords = explode(' ', $string);
+        foreach ($arrayWords as $key => $value) {
+            if($value == ""){
+                unset($arrayWords[$key]);
+            }
+        }
+
+        return $arrayWords;
+    }
+
+
+    /**
+     * Get Count From Words
+     * @access public
+     * @param string $string
+     * @return array
+     */
+    public function getCountFromWords(array $words): array
+    {
+        $wordsWithCount = [];
+        foreach ($words as $key => $value) {
+            $wordsWithCount[$key]['len'] = mb_strlen($value, 'utf8');
+            $wordsWithCount[$key]['word'] = $value;
+        }
+
+        return $wordsWithCount;
     }
 
     /**
@@ -96,9 +171,9 @@ class TweetService
     /**
      * Get mock tweets
      * @access public
-     * @return object
+     * @return array
      */
-    public function getMockTweet(): object
+    public function getMockTweet(): array
     {
         return json_decode(file_get_contents(public_path('default.json')));
     }
